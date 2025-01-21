996dd04892d4bcfae5891d14e9f0257577250b79
Lukas <lukaskarstens07@gmail.com>
2025-01-21 14:13:26 +0100

    About Page

--- src/pages/about/about.styled.js
+++ src/pages/about/about.styled.js
@@ -1,3 +1,19 @@
 import styled from "styled-components";
 
-export const AboutStyled = styled.div``;
+export const AboutStyled = styled.div`
+    flex: 1;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 40px;
+
+    div {
+        p {
+            margin-bottom: 20px;
+            font-size: 1.4rem;
+        }
+    }
+
+    img {
+        width: 100%;
+    }
+`;
